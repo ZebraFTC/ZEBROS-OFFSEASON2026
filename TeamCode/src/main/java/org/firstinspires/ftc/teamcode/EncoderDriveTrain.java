6449ccb007e4d9486b95cf24d7c026704ea64e1c
ZLT06 <zebolts30416@gmail.com>
2026-06-27 11:31:44 -0400

    Encoders changed for linkage

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderDriveTrain.java
@@ -13,7 +13,7 @@ public class EncoderDriveTrain extends LinearOpMode {
     public DcMotor BackRight;
     public DcMotor Retraction;
     public  DcMotor Extension;
-    int revolution = 752;
+    int revolution = 538;
     @Override
     public void runOpMode() {
         FrontLeft = hardwareMap.get(DcMotor.class, "FL");
@@ -24,6 +24,7 @@ public class EncoderDriveTrain extends LinearOpMode {
         Extension = hardwareMap.get(DcMotor.class, "EX");
         FrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         BackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        Retraction.setDirection(DcMotorSimple.Direction.REVERSE);
 
         Extension.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         Retraction.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -35,59 +36,56 @@ public class EncoderDriveTrain extends LinearOpMode {
             double drive = -0.5*gamepad1.left_stick_y;
             double strafe = -0.5*gamepad1.left_stick_x;
             double turn = 0.5*gamepad1.right_stick_x;
-            FrontLeft.setPower(drive+turn-strafe);
-            FrontRight.setPower(drive-turn+strafe);
-            BackLeft.setPower(drive+turn+strafe);
-            BackRight.setPower(drive-turn-strafe);
+            FrontLeft.setPower(drive+turn+strafe);
+            FrontRight.setPower(drive-turn-strafe);
+            BackLeft.setPower(drive+turn-strafe);
+            BackRight.setPower(drive-turn+strafe);
 
-            double extend = gamepad1.left_trigger-gamepad1.right_trigger;
-
-            if (extend > 0.5) {
-                Extension.setTargetPosition(Extension.getCurrentPosition() + 3*(revolution));
-                Retraction.setTargetPosition(Retraction.getCurrentPosition() + 3*(revolution));
+            if (gamepad1.left_trigger > 0.2) {
+                Extension.setTargetPosition(Extension.getCurrentPosition() + (135));
+                Retraction.setTargetPosition(Retraction.getCurrentPosition() + (135));
 
                 Extension.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 Retraction.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-                Extension.setPower(0.2);
-                Retraction.setPower(0.2);
+                Extension.setPower(0.75);
+                Retraction.setPower(0.75);
 
                 double timeout = getRuntime() + 5.0;
 
-                while (opModeIsActive()
-                        && (Extension.isBusy()
-                        || Retraction.isBusy()
-                        && getRuntime() < timeout)) {
-
-                    telemetry.addLine("Extending");
-                }
-
-                Extension.setPower(0);
-                Retraction.setPower(0);
-
-            }
-            if (extend < -0.5) {
-                Extension.setTargetPosition(Extension.getCurrentPosition() - 3*(revolution));
-                Retraction.setTargetPosition(Retraction.getCurrentPosition() - 3*(revolution));
-
-                Extension.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                Retraction.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-                Extension.setPower(0.2);
-                Retraction.setPower(0.2);
-
-                double timeout = getRuntime() + 5.0;
-
-                while (opModeIsActive()
-                        && (Extension.isBusy()
+                if ((Extension.isBusy()
                         || Retraction.isBusy()
                         && getRuntime() < timeout)) {
 
                     telemetry.addLine("Retracting");
+                } else {
+                    Extension.setPower(0);
+                    Retraction.setPower(0);
                 }
 
-                Extension.setPower(0);
-                Retraction.setPower(0);
+
+            }
+            if (gamepad1.right_trigger > 0.2) {
+                Extension.setTargetPosition(Extension.getCurrentPosition() - (135));
+                Retraction.setTargetPosition(Retraction.getCurrentPosition() - (135));
+
+                Extension.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                Retraction.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                Extension.setPower(1);
+                Retraction.setPower(1);
+
+                double timeout = getRuntime() + 5.0;
+
+                if ((Extension.isBusy()
+                        || Retraction.isBusy()
+                        && getRuntime() < timeout)) {
+
+                    telemetry.addLine("Extending");
+                } else {
+                    Extension.setPower(0);
+                    Retraction.setPower(0);
+                }
             }
 
         }
