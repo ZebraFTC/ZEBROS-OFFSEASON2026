e60e8082c28525ea7c87fe339d0dae4da20e56b1
ZLT08 <zebros30415@gmail.com>
2026-08-13 19:29:32 -0400

    camera

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Camera.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.Mechanisms;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+
+public class Camera {
+}
